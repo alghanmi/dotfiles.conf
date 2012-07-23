363d6286b6be79b4989fe9fe69edb05ca6b450de
Rami Al-Ghanmi <rami@ramialghanmi.com>
2012-07-22 23:39:19 -0700

    Fixed bug in $PATH assignment

--- java_home/java_home-setup.sh
+++ java_home/java_home-setup.sh
@@ -15,6 +15,6 @@ echo "" >> ~/.bashrc
 echo "##Java Setup" >> ~/.bashrc
 echo "export JAVA_HOME=$JAVA_HOME_LOC" >> ~/.bashrc
 echo "export CLASSPATH=." >> ~/.bashrc
-echo "PATH=$PATH:$JAVA_HOME/bin" >> ~/.bashrc
+echo 'PATH=$PATH:$JAVA_HOME/bin' >> ~/.bashrc
 
 ## TODO: Add a directory for java libraries to include in classpath
